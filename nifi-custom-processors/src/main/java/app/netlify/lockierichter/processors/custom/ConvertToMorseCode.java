167382151a1d36ab009039078813c5ae1fc96145
Lockie Richter <richter.lockie@gmail.com>
2021-06-05 00:08:32 +0930

    Created logic for converting to morse code
    Also created unit tests, added JaCoCo to the Maven test phase and did
    some minor refactoring.

--- nifi-custom-processors/src/main/java/app/netlify/lockierichter/processors/custom/ConvertToMorseCode.java
+++ nifi-custom-processors/src/main/java/app/netlify/lockierichter/processors/custom/ConvertToMorseCode.java
@@ -1,26 +1,5 @@
 package app.netlify.lockierichter.processors.custom;
 
-import org.apache.nifi.components.PropertyDescriptor;
-import org.apache.nifi.flowfile.FlowFile;
-import org.apache.commons.io.IOUtils;
-import org.apache.nifi.annotation.behavior.ReadsAttribute;
-import org.apache.nifi.annotation.behavior.ReadsAttributes;
-import org.apache.nifi.annotation.behavior.WritesAttribute;
-import org.apache.nifi.annotation.behavior.WritesAttributes;
-import org.apache.nifi.annotation.lifecycle.OnScheduled;
-import org.apache.nifi.annotation.documentation.CapabilityDescription;
-import org.apache.nifi.annotation.documentation.SeeAlso;
-import org.apache.nifi.annotation.documentation.Tags;
-import org.apache.nifi.processor.exception.ProcessException;
-import org.apache.nifi.processor.io.InputStreamCallback;
-import org.apache.nifi.processor.io.OutputStreamCallback;
-import org.apache.nifi.processor.AbstractProcessor;
-import org.apache.nifi.processor.ProcessContext;
-import org.apache.nifi.processor.ProcessSession;
-import org.apache.nifi.processor.ProcessorInitializationContext;
-import org.apache.nifi.processor.Relationship;
-import org.apache.nifi.processor.util.StandardValidators;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -32,32 +11,48 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
-@Tags({ "example" })
-@CapabilityDescription("Provide a description")
-@SeeAlso({})
-@ReadsAttributes({ @ReadsAttribute(attribute = "", description = "") })
-@WritesAttributes({ @WritesAttribute(attribute = "", description = "") })
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.ProcessorInitializationContext;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.io.InputStreamCallback;
+import org.apache.nifi.processor.io.OutputStreamCallback;
+
+@Tags({ "Morse", "Code", "Converter" })
+@CapabilityDescription("Converts the text in a FlowFile to Morse Code and writes it back out.")
 public class ConvertToMorseCode extends AbstractProcessor {
 
-    public static final PropertyDescriptor MY_PROPERTY = new PropertyDescriptor.Builder().name("MY_PROPERTY")
-            .displayName("My property").description("Example Property").required(true)
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR).build();
-
-    public static final Relationship MY_RELATIONSHIP = new Relationship.Builder().name("MY_RELATIONSHIP")
-            .description("Example relationship").build();
+    public static final Relationship REL_SUCCESS = new Relationship.Builder().name("Success")
+            .description("Successfully converted FlowFiles will be routed to this relationship.").build();
 
     private List<PropertyDescriptor> descriptors;
 
     private Set<Relationship> relationships;
 
+    // Characters that we can convert to morse code
+    private static char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+
+    // Morse code characters in alphabetical order
+    private static String[] morseLetters = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+            "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
+            "--..", "|" };
+
     @Override
     protected void init(final ProcessorInitializationContext context) {
-        final List<PropertyDescriptor> descriptors = new ArrayList<>();
-        descriptors.add(MY_PROPERTY);
+        descriptors = new ArrayList<>();
         this.descriptors = Collections.unmodifiableList(descriptors);
 
-        final Set<Relationship> relationships = new HashSet<>();
-        relationships.add(MY_RELATIONSHIP);
+        relationships = new HashSet<>();
+        relationships.add(REL_SUCCESS);
         this.relationships = Collections.unmodifiableSet(relationships);
     }
 
@@ -71,11 +66,6 @@ public class ConvertToMorseCode extends AbstractProcessor {
         return descriptors;
     }
 
-    @OnScheduled
-    public void onScheduled(final ProcessContext context) {
-
-    }
-
     @Override
     public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
         FlowFile flowFile = session.get();
@@ -85,6 +75,7 @@ public class ConvertToMorseCode extends AbstractProcessor {
 
         final AtomicReference<String> flowFileText = new AtomicReference<>();
 
+        // To read the contents of the FlowFile
         session.read(flowFile, new InputStreamCallback() {
             @Override
             public void process(InputStream in) throws IOException {
@@ -93,7 +84,7 @@ public class ConvertToMorseCode extends AbstractProcessor {
             }
         });
 
-        // To write the results back out ot flow file
+        // To write the results back out to FlowFile
         flowFile = session.write(flowFile, new OutputStreamCallback() {
 
             @Override
@@ -103,9 +94,51 @@ public class ConvertToMorseCode extends AbstractProcessor {
             }
         });
 
+        session.transfer(flowFile, REL_SUCCESS);
+
     }
 
-    protected String convertTextToMorseCode(String text) {
-        return null;
+    /**
+     * Takes a blob of text and converts it to morse code. Words are separated by
+     * seven dots.
+     *
+     * @param text the text blob to convert.
+     * @return the converted text blob as morse code.
+     */
+    protected static String convertTextToMorseCode(String text) {
+        // Remove all punctuation.
+        String[] tokens = text.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase().split("\\s+");
+        List<String> convertedTokens = new ArrayList<>();
+
+        // Loop over each word and convert it to morse code.
+        for (String token : tokens) {
+            String convertedToken = convertWordToMorseCode(token);
+            convertedTokens.add(convertedToken);
+        }
+
+        // Return the converted tokens as a single string, separated by seven dots.
+        return String.join(" ....... ", convertedTokens);
+    }
+
+    /**
+     * Converts a given word to morse code.
+     *
+     * @param word the word to convert.
+     * @return the converted word in morse code.
+     */
+    protected static String convertWordToMorseCode(String word) {
+        StringBuilder sb = new StringBuilder();
+        // Loop over the characters in our word and get the index of the letter.
+        for (char c : word.toLowerCase().toCharArray()) {
+            int index = ArrayUtils.indexOf(letters, c);
+            // An index of -1 means that the character wasn't found in our letters array.
+            if (index != -1) {
+                String morseChar = morseLetters[index];
+                sb.append(morseChar);
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString().trim();
     }
 }
--- nifi-custom-processors/src/test/java/app/netlify/lockierichter/processors/custom/ConvertToMorseCodeTest.java
+++ nifi-custom-processors/src/test/java/app/netlify/lockierichter/processors/custom/ConvertToMorseCodeTest.java
@@ -1,38 +1,84 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package app.netlify.lockierichter.processors.custom;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.nifi.util.MockFlowFile;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class ConvertToMorseCodeTest {
+class ConvertToMorseCodeTest {
 
     private TestRunner testRunner;
 
-    @BeforeAll
-    public void init() {
+    @BeforeEach
+    void init() {
         testRunner = TestRunners.newTestRunner(ConvertToMorseCode.class);
     }
 
-    @Test
-    public void testProcessor() {
+    private static Stream<Arguments> testProcessor() {
+        return Stream.of(Arguments.of("BasicTextFlowFile", "BasicMorseCodeFlowFile"));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testProcessor(String inputFile, String expectedFile) throws IOException {
         testRunner.assertQueueEmpty();
+        testRunner.run();
+        testRunner.assertQueueEmpty();
+
+        InputStream input = getClass().getClassLoader().getResourceAsStream(inputFile);
+        testRunner.enqueue(input);
+        testRunner.assertQueueNotEmpty();
+        testRunner.run();
+        testRunner.assertQueueEmpty();
+
+        int expectedLength = 1;
+
+        List<MockFlowFile> successFiles = testRunner.getFlowFilesForRelationship(ConvertToMorseCode.REL_SUCCESS);
+        assertEquals(expectedLength, successFiles.size());
+
+        MockFlowFile result = successFiles.get(0);
+        InputStream expectedContentStream = getClass().getClassLoader().getResourceAsStream(expectedFile);
+        String expectedContent = IOUtils.toString(expectedContentStream, StandardCharsets.UTF_8.name());
+        result.assertContentEquals(expectedContent);
+
+    }
+
+    private static Stream<Arguments> testConvertWordToMorseCode() {
+        return Stream.of(Arguments.of("Alice", ".- .-.. .. -.-. ."), Arguments.of("and", ".- -. -.."),
+                Arguments.of("Bob", "-... --- -..."), Arguments.of("", ""), Arguments.of("       ", ""),
+                Arguments.of("   -- { _}    ", ""));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testConvertWordToMorseCode(String input, String expected) {
+        String actual = ConvertToMorseCode.convertWordToMorseCode(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> testConvertTextToMorseCode() {
+        return Stream.of(Arguments.of("Alice and Bob", ".- .-.. .. -.-. . ....... .- -. -.. ....... -... --- -..."),
+                Arguments.of("", ""), Arguments.of("       ", ""), Arguments.of("   -- { _}    ", ""));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testConvertTextToMorseCode(String input, String expected) {
+        String actual = ConvertToMorseCode.convertTextToMorseCode(input);
+        Assertions.assertEquals(expected, actual);
     }
 
 }
